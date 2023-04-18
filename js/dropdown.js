d6fbf1008b82b9e7e3f4bf903faef8be9f8c17c2
SOFTWARE <SOFTWARE@YOPDFRCN10F922.FormacionSena.RED>
2023-04-18 12:03:14 -0500

    nav

---
+++ js/dropdown.js
@@ -0,0 +1,25 @@
+
+const usuariosTitle = document.querySelector('.drop_title');
+const usuariosList = document.querySelector('.drop_list');
+const dropArrow = document.querySelector('.drop_arrow');
+
+usuariosTitle.addEventListener('click', () => {
+    if(usuariosTitle.style.backgroundColor != 'red'){
+        usuariosTitle.style.backgroundColor = 'red';
+    }else{
+        usuariosTitle.style.backgroundColor = '#24272F';
+    }
+    usuariosList.classList.toggle('active');
+    dropArrow.classList.toggle('drop_arrow--rotate');
+});
+
+//segundo dropdown
+
+const productoTitle = document.querySelector('.drop_title_product');
+const productoList = document.querySelector('.drop_list_product');
+const dropArrow2 = document.querySelector('.drop_arrow2');
+
+productoTitle.addEventListener('click', () => {
+    productoList.classList.toggle('active2');
+    dropArrow2.classList.toggle('drop_arrow--rotate2');
+});
